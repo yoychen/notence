0f13e188d66b6c736102be1e74138019f1f2142b
yoychen <yui12327@gmail.com>
2020-05-11 10:02:01 +0000

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import rootReducer from "./reducers";
+
+export default configureStore({ reducer: rootReducer });
