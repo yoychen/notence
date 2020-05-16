80873765aaad443f4a396b15db1c8cb0f9893ab1
yoychen <yui12327@gmail.com>
2020-05-16 10:05:57 +0000

    Fix hot reloading on redux

--- src/store.js
+++ src/store.js
@@ -2,4 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./reducers";
 
-export default configureStore({ reducer: rootReducer });
+const store = configureStore({ reducer: rootReducer });
+
+if (module.hot) {
+  module.hot.accept("./reducers", () => {
+    // eslint-disable-next-line global-require
+    const nextRootReducer = require("./reducers").default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
